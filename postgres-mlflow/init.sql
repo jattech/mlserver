7e966abaefbe9ebd13781552eb51764cf8d2ae18
jose <joseantonio.torre@MAC-Jose-Antonio.local>
2022-02-06 22:26:29 +0100

    volumes

--- postgres-mlflow/init.sql
+++ postgres-mlflow/init.sql
@@ -1,2 +1,3 @@
-CREATE DATABASE mlflow_db;
+SELECT 'CREATE DATABASE mlflow_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'mlflow_db')\gexec
 GRANT ALL PRIVILEGES ON DATABASE mlflow_db TO mlflow_user;
